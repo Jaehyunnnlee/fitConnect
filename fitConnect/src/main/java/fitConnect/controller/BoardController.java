96780884d53630a55d59560cc125176e72426ed7
Jaehyunnnlee <inv1452@naver.com>
2024-05-30 20:19:04 +0900

    특정 NickName이 올린 게시글 조회 기능 추가

--- fitConnect/src/main/java/fitConnect/controller/BoardController.java
+++ fitConnect/src/main/java/fitConnect/controller/BoardController.java
@@ -1,10 +1,12 @@
 package fitConnect.controller;
 
-import lombok.RequiredArgsConstructor;
 import fitConnect.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,7 +22,13 @@ public class BoardController {
     public String readPost(){
         return "board/readPost";
     }
-    @GetMapping("/my-posts") String myPosts(){
+    @GetMapping("/my-posts") public String myPosts(){
+        return "board/my-posts-bootstrap";
+    }
+    @GetMapping("{userName}-posts")
+    public String selectByUserName(@PathVariable("userName")String userName, Model model){
+
+        model.addAttribute("userName",userName);
         return "board/my-posts-bootstrap";
     }
     @GetMapping("/post/{postNum}")
@@ -28,6 +36,7 @@ public class BoardController {
         model.addAttribute("postNum",postNum);
         return "board/read-bootstrap";
     }
+
     @GetMapping("/post/{postNum}/edit")
     public String updatePost(@PathVariable("postNum")Long postNum,Model model){
         model.addAttribute("postNum",postNum);
