ad4226c3ca6c1c7ced8d3d456db0df19dccaf047
Jaehyunnnlee <inv1452@naver.com>
2024-05-30 19:50:41 +0900

    RoutineRequestDto 추가

---
+++ fitConnect/src/main/java/fitConnect/controller/dto/RoutineRequestDto.java
@@ -0,0 +1,20 @@
+package fitConnect.controller.dto;
+
+import fitConnect.entity.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoutineRequestDto {
+    private String routineTitle;
+    private User user;
+    private String userId;
+    private String routineDescription;
+    private boolean completed;
+
+    public void setUser(User user){
+        this.user=user;
+        this.userId=user.getUserId();
+    }
+}
