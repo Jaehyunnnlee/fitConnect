dd46e61bf968d14ba1b7887975c768497d46e54c
Jaehyunnnlee <inv1452@naver.com>
2024-05-30 20:24:29 +0900

    sign-up validation을 위해 @NotBlank와 Pattern 수정

--- fitConnect/src/main/java/fitConnect/controller/dto/UserRequestDto.java
+++ fitConnect/src/main/java/fitConnect/controller/dto/UserRequestDto.java
@@ -1,17 +1,26 @@
 package fitConnect.controller.dto;
 
+import fitConnect.entity.user.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
-import fitConnect.entity.user.Role;
 
 @Getter
 @Setter
 public class UserRequestDto {
-    private Long userNum;
+    @NotBlank(message="아이디는 필수 입력 값입니다.")
     private String userId;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String userPwd;
+    @NotBlank(message = "닉네임은 필수입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String userName;
     private String userPwdConfirm;
     private Role role;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
 }
