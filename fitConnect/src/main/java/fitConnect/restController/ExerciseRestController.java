351098cbc6eb0a7d0f9b48bb57c985201d92899b
Jaehyunnnlee <inv1452@naver.com>
2024-05-30 20:11:05 +0900

    Exercise rest-api 추가

---
+++ fitConnect/src/main/java/fitConnect/restController/ExerciseRestController.java
@@ -0,0 +1,24 @@
+package fitConnect.restController;
+
+import fitConnect.controller.dto.response.ExerciseResponseDto;
+import fitConnect.service.ExerciseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class ExerciseRestController {
+    private final ExerciseService exerciseService;
+    @GetMapping("/rest-api/exercises/{exercisePart}")
+    public ResponseEntity<?> readExercises(@PathVariable("exercisePart") String exercisePart){
+        List<ExerciseResponseDto> exerciseList=exerciseService.getExercise(exercisePart);
+        return ResponseEntity.ok(exerciseList);
+    }
+}
