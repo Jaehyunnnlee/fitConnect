39b1a342b04601011dd9ddd18d89902251f94331
Jaehyunnnlee <inv1452@naver.com>
2024-05-30 20:31:50 +0900

    회원가입 시 @valid 어노테이션을 이용한 에러 처리 기능 추가

--- fitConnect/src/main/java/fitConnect/service/UserService.java
+++ fitConnect/src/main/java/fitConnect/service/UserService.java
@@ -1,47 +1,52 @@
 package fitConnect.service;
 
-import lombok.RequiredArgsConstructor;
 import fitConnect.controller.dto.UserRequestDto;
 import fitConnect.controller.dto.response.UserResponseDto;
 import fitConnect.entity.user.CustomUserDetails;
 import fitConnect.entity.user.Role;
 import fitConnect.entity.user.User;
 import fitConnect.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class UserService{
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public UserResponseDto signupAccount(UserRequestDto requestDto){
-        String userId=requestDto.getUserId();
-        String userName=requestDto.getUserName();
-        if(userRepository.existsByUserId(userId)){
-            throw new IllegalArgumentException("이미 사용 중인 아이디입니다.");
+    @Transactional(rollbackFor = Exception.class)
+    public UserResponseDto signupAccount(@Valid UserRequestDto requestDto) {
+        try {
+            String userId = requestDto.getUserId();
+            String userName = requestDto.getUserName();
+            if (userRepository.existsByUserId(userId)) {
+                throw new IllegalArgumentException("이미 사용중인 아이디입니다.");
+            } else if (userRepository.existsByUserName(userName)) {
+                throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+            } else if (!requestDto.getUserPwd().equals(requestDto.getUserPwdConfirm())) {
+                throw new IllegalArgumentException("비밀번호가 서로 일치하지 않습니다.");
+            }
+            requestDto.setUserPwd(passwordEncoder.encode(requestDto.getUserPwd()));
+            User user = new User(requestDto);
+            if (userId.equals("admin")) {
+                user.setRole(Role.ADMIN);
+            } else {
+                user.setRole(Role.USER);
+            }
+            userRepository.save(user);
+            return new UserResponseDto(user);
+        } catch (IllegalArgumentException ex) {
+            // 예외를 잡아서 다시 던져줍니다.
+            throw ex;
         }
-        else if(userRepository.existsByUserName(userName)){
-            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
-        }
-        else if(!requestDto.getUserPwd().equals(requestDto.getUserPwdConfirm())){
-            throw new IllegalArgumentException("비밀번호가 서로 일치하지 않습니다.");
-        }
-        requestDto.setUserPwd(passwordEncoder.encode(requestDto.getUserPwd()));
-        User user=new User(requestDto);
-        if(userId.equals("admin")){
-            user.setRole(Role.ADMIN);
-        }
-        else{
-            user.setRole(Role.USER);
-        }
-        userRepository.save(user);
-        return new UserResponseDto(user);
     }
 
     @Transactional
@@ -82,6 +87,5 @@ public class UserService{
         User user=userRepository.findByUserId(userId).orElseThrow(()->new UsernameNotFoundException("로그인 하세요"));
         return new UserResponseDto(user);
     }
-
 
 }
