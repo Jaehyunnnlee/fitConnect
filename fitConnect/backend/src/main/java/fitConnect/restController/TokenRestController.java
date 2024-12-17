a68feedea6d024817227e01d46538507987beca4
Jaehyunnnlee <inv1452@naver.com>
2024-12-17 23:12:36 +0900

    feat: JWT AccessToken Controller 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/restController/TokenRestController.java
@@ -0,0 +1,25 @@
+package fitConnect.restController;
+
+
+import fitConnect.controller.dto.AccessTokenRequestDto;
+import fitConnect.controller.dto.response.AccessTokenResponseDto;
+import fitConnect.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/rest-api/token")
+public class TokenRestController {
+    private final TokenService tokenService;
+    @PostMapping
+    public ResponseEntity<AccessTokenResponseDto> createNewAccessToken(@RequestBody AccessTokenRequestDto requestDto){
+        String newAccessToken=tokenService.createNewAccessToken(requestDto.getRefreshToken());
+        return ResponseEntity.status(HttpStatus.CREATED).body(new AccessTokenResponseDto(newAccessToken));
+    }
+}
