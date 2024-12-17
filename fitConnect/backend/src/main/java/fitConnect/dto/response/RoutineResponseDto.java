1abb54a402f4445f3bfbeb51c6908a4316c32759
Jaehyunnnlee <inv1452@naver.com>
2024-12-18 00:41:59 +0900

    feat: RoutineResponseDto 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/dto/response/RoutineResponseDto.java
@@ -0,0 +1,27 @@
+package fitConnect.dto.response;
+
+import fitConnect.entity.Routine;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class RoutineResponseDto {
+    private Long routineNum;
+    private String routineTitle;
+    private String routineDescription;
+    private String userName;
+    private String userId;
+    private LocalDateTime createdAt;
+    private boolean completed;
+
+    public RoutineResponseDto(Routine entity){
+        this.routineNum=entity.getRoutineNum();
+        this.routineTitle=entity.getRoutineTitle();
+        this.routineDescription=entity.getRoutineDescription();
+        this.userName=entity.getUser().getUserName();
+        this.userId=entity.getUser().getUserId();
+        this.createdAt=entity.getCreateAt();
+        this.completed=entity.isCompleted();
+    }
+}
