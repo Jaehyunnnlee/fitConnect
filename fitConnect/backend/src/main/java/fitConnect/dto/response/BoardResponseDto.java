b4065d9a362fb647f491b748da491b67a5ea2567
Jaehyunnnlee <inv1452@naver.com>
2024-12-18 00:37:52 +0900

    feat: BoardResponseDto 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/dto/response/BoardResponseDto.java
@@ -0,0 +1,25 @@
+package fitConnect.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import fitConnect.entity.Board;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class BoardResponseDto {
+    private Long postNum;
+    private String postTitle;
+    private String postContent;
+    private String userName;
+    private LocalDateTime modifiedAt;
+
+    public BoardResponseDto(Board entity){
+        this.postNum=entity.getPostNum();
+        this.postTitle=entity.getPostTitle();
+        this.postContent=entity.getPostContent();
+        this.userName=entity.getUser().getUserName();
+        this.modifiedAt = entity.getModifiedAt();
+    }
+}
