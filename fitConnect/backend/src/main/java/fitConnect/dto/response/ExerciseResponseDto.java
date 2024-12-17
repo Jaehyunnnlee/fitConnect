aa447d5696d2b74933f80d1a163c7f142a8328f0
Jaehyunnnlee <inv1452@naver.com>
2024-12-18 00:40:50 +0900

    feat: ExerciseResponseDto 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/dto/response/ExerciseResponseDto.java
@@ -0,0 +1,19 @@
+package fitConnect.dto.response;
+
+import fitConnect.entity.Exercise;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExerciseResponseDto {
+    private Long exerciseNum;
+    private String exerciseName;
+    private String exercisePart;
+
+    public ExerciseResponseDto(Exercise entity){
+        this.exerciseNum=entity.getExerciseNum();
+        this.exerciseName=entity.getExerciseName();
+        this.exercisePart=entity.getExercisePart();
+    }
+}
