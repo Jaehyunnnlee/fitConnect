95e2adb7dcf8e7f0e8f8a6d5725028479c7d0112
Jaehyunnnlee <inv1452@naver.com>
2024-12-17 23:16:22 +0900

    feat: JWT TokenService 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/service/TokenService.java
@@ -0,0 +1,27 @@
+package fitConnect.service;
+
+import fitConnect.config.security.JwtProvider;
+import fitConnect.controller.dto.response.UserResponseDto;
+import fitConnect.entity.user.User;
+import fitConnect.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserRepository userRepository;
+
+    public String createNewAccessToken(String refreshToken){
+        if(!jwtProvider.validateToken(refreshToken)){
+            throw new IllegalArgumentException("unexpected token");
+        }
+
+        String userId=refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user=userRepository.findByUserId(userId).orElseThrow(()->new IllegalArgumentException("unexpected user"));
+        UserResponseDto userResponseDto=new UserResponseDto(user);
+        return jwtProvider.createToken(userResponseDto);
+    }
+}
