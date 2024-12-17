31f737273959c1dce27738346d7bd41116a17b88
Jaehyunnnlee <inv1452@naver.com>
2024-12-18 00:41:29 +0900

    feat: ExerciseRoutineResponseDto 추가

---
+++ fitConnect/backend/src/main/java/fitConnect/dto/response/ExerciseRoutineResponseDto.java
@@ -0,0 +1,24 @@
+package fitConnect.dto.response;
+
+import fitConnect.entity.exerciseRoutine.ExerciseRoutine;
+import lombok.Getter;
+
+
+@Getter
+public class ExerciseRoutineResponseDto {
+    private Long routineNum;
+    private Long exerciseNum;
+    private String exerciseName;
+    private int sets;
+    private int reps;
+    private int weight;
+
+    public ExerciseRoutineResponseDto(ExerciseRoutine entity){
+        this.routineNum=entity.getExerciseRoutinePK().getRoutine().getRoutineNum();
+        this.exerciseNum=entity.getExerciseRoutinePK().getExercise().getExerciseNum();
+        this.exerciseName=entity.getExerciseRoutinePK().getExercise().getExerciseName();
+        this.sets=entity.getSets();
+        this.reps=entity.getReps();
+        this.weight=entity.getWeight();
+    }
+}
